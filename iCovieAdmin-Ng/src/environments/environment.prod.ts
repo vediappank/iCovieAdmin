00dcdc4181482953b9f2b40849feda0a2ac50b28
vediappan <vediappanbe@gmail.com>
2020-08-08 20:47:30 +0300

    da
    update

---
+++ iCovieAdmin-Ng/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+	production: true,
+	isMockEnabled: true, // You have to switch this, when your real back-end is done	
+	authTokenKey:'',
+	baseUrl: 'http://13.127.139.216:80/CovieAdminApi',
+	//baseUrl: 'http://localhost:58386/'
+};
